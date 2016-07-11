8061ad25054cc7a50071f449edc35f6e2fb59b2a
Sandip <sandip.necka@gmail.com>
2016-07-11 16:46:11 -0500

    fixed issue on BackendApplication

--- backend/src/main/java/com/openmart/BackendApplication.java
+++ backend/src/main/java/com/openmart/BackendApplication.java
@@ -1,8 +1,6 @@
 package com.openmart;
 
-import com.openmart.core.business.user.controller.UserController;
-import com.openmart.core.business.user.model.Login;
-import com.openmart.core.business.user.model.User;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BackendApplication.class, args);
         ApplicationContext context = SpringApplication.run(BackendApplication.class, args);
-        UserController userController = context.getBean("userController", UserController.class);
-        userController.test();
-        userController.addUserService(new User("Nawa Raj Acharya", new Login("username", "password")));
 	}
 }
