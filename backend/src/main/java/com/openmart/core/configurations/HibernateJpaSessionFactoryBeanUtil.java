0a0c298f8add355f339820791a045cc545b6ddf1
Nawa Acharya <git@gitlab.com:nawa.acharya04/openMart.git>
2016-07-11 16:35:17 -0500

    hibernate jpa session factory bean config class....

--- backend/src/main/java/com/openmart/core/configurations/HibernateJpaSessionFactoryBeanUtil.java
+++ backend/src/main/java/com/openmart/core/configurations/HibernateJpaSessionFactoryBeanUtil.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * Created by Nawa on 7/11/2016.
  */
+//config file for hibernate sesion factory
 @Configuration
 @EnableTransactionManagement
 public class HibernateJpaSessionFactoryBeanUtil {
