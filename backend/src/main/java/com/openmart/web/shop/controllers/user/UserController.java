bfa306941878f125161ca368ab3fbc628b961a12
Nawa Acharya <git@gitlab.com:nawa.acharya04/openMart.git>
2016-07-12 21:41:35 -0500

    user controller updated

---
+++ backend/src/main/java/com/openmart/web/shop/controllers/user/UserController.java
@@ -0,0 +1,43 @@
+package com.openmart.web.shop.controllers.user;
+
+import com.openmart.core.business.user.model.*;
+import com.openmart.core.business.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Nawa on 7/10/2016.
+ */
+//controller user
+@RestController("openmart/api")
+@CrossOrigin(origins = "http://localhost:8080")
+public class UserController {
+   @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public @ResponseBody User registerUser(@RequestBody User user){
+        System.out.println("inside register user in controller ****************************");
+        userService.addUser(user);
+        return user;
+    }
+
+    @RequestMapping(value= "/{username}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody User showUserDetails(@RequestParam String username){
+        return userService.getUser(username);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public @ResponseBody User getUserFromUsername(@RequestBody Login login){
+        System.out.println(login.toString());
+        System.out.println("inside login in controller **********************************");
+        return userService.findUser(login);
+    }
+
+    @RequestMapping(value = "profile/update/{username}", method = RequestMethod.POST)
+    public @ResponseBody User updateProfile(@RequestBody Profile profile, @PathVariable String username) {
+        System.out.println("inside update use details in controller **********************************");
+        userService.addUpdatesToUser(profile, username);
+        return userService.getUser(username);
+    }
+}
