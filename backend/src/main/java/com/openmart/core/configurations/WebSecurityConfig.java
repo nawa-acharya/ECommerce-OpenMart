616da565cdeec199590f023789efe15d8df65c10
Sandip <sandip.necka@gmail.com>
2016-07-13 10:22:59 -0500

    codes to secure api

--- backend/src/main/java/com/openmart/core/configurations/WebSecurityConfig.java
+++ backend/src/main/java/com/openmart/core/configurations/WebSecurityConfig.java
@@ -1,27 +1,39 @@
 package com.openmart.core.configurations;
 
+import com.openmart.core.security.AppUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 /*
  * Created by Sandip on 7/3/16.
 */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+     @Autowired
+     AppUserDetailService userDetailService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-        http.csrf().disable();
+        http
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/**").hasRole("CUSTOMER");
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      //  auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-    }
+
 }
