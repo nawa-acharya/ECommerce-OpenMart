f60cdd3fa63a37e8b71f1c7b734f2da8b9e32764
Sandip <sandip.necka@gmail.com>
2016-07-14 02:40:01 -0500

    access control list security enabled in frontend and #bug user service #bug mockup data

--- frontend/src/main/resources/static/app/js/controllers/thankyou.js
+++ frontend/src/main/resources/static/app/js/controllers/thankyou.js
@@ -3,8 +3,39 @@
  */
 
 
-function thankyouController(){
+function thankyouController($scope,$tate,DataService){
+    DataService.cart.clearItems();
 
 
 
 }
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#menu-toggle-2").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled-2");
+    $('#menu ul').hide();
+});
+
+function initMenu() {
+    $('#menu ul').hide();
+    $('#menu ul').children('.current').parent().show();
+    //$('#menu ul:first').show();
+    $('#menu li a').click(
+        function() {
+            var checkElement = $(this).next();
+            if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                return false;
+            }
+            if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                $('#menu ul:visible').slideUp('normal');
+                checkElement.slideDown('normal');
+                return false;
+            }
+        }
+    );
+}
+$(document).ready(function() {initMenu();});
